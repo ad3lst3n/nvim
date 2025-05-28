d8e397965d0c3cc442cfcbff81791a5eee2e9883
aa224rw <aa224rw@student.lnu.se>
2025-05-28 23:26:27 +0200

    Aerial and Undotree

--- init.lua
+++ init.lua
@@ -15,6 +15,16 @@ require("plugins.colorscheme")
 require("options")
 require("lazy").setup("plugins")
 
+require("aerial").setup({
+  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+  on_attach = function(bufnr)
+    -- Jump forwards/backwards with '{' and '}'
+    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+  end,
+})
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+
 vim.diagnostic.config({
   virtual_text = true,
 
